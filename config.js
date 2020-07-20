87a3fcfb2ef90e92cca7763eda50c092edf72e52
ebube <ebube1995@gmail.com>
2020-07-20 20:27:48 +0200

    removed my details from the config file

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports ={
     ENV:process.env.NODE_ENV || 'develop',
     PORT:process.env.PORT || 3000,
     URL:process.env.BASE_URL || 'http://localhost:3000',
-    MONGOB_URI:process.env.MONGOB_URI || 'mongodb+srv://ebube:Ebube1234@cluster0.zgif5.mongodb.net/test'
+    MONGOB_URI:process.env.MONGOB_URI || 'add yours here '
 }
